b210665e437240f1167e30a385b74e97d649d70d
ddujeong <qoddl159@nate.com>
2025-10-20 09:13:18 +0900

    api 아이피 주소 추가

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8888", // 스프링 부트 백엔드 기본 url
-    withCredentials: true // 세션 쿠키 전달
+  baseURL: "http://localhost:8888", // 스프링 부트 백엔드 기본 url
+  // "http://172.30.1.76:8888/api"
+  withCredentials: true, // 세션 쿠키 전달
 });
 
 export default api;
